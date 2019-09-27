101bf87e7e345991751b2fc48181f5f04c6d0e2a
Paul Vidal <paul.vid@gmail.com>
2019-09-27 17:14:46 -0400

    Fixed minor template issue

--- scripts/aws/tez_log_command.sh
+++ scripts/aws/tez_log_command.sh
@@ -5,7 +5,7 @@ hdfs dfs -copyToLocal /nifi/app_data
 hdfs dfs -copyToLocal /nifi/dag_data
 hdfs dfs -copyToLocal /nifi/dag_meta
 
-export NOW=$(date +"%m-%d-%Y")
+export NOW=$(date +"%s")
 
 tar -czf tez-payloads-$NOW.tar.gz app_data dag_data dag_meta
 
