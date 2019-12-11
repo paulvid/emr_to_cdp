f2ff36b60aaa7051eddadd1c6f3e977cbe48290d
Paul Vidal <paul.vid@gmail.com>
2019-12-11 10:26:24 -0500

    Reviewed bucket creation

--- scripts/cdp/aws-pre-req/aws_create_bucket.sh
+++ scripts/cdp/aws-pre-req/aws_create_bucket.sh
@@ -41,19 +41,15 @@ then
 fi 
 
 
-if [ $(aws s3api head-bucket --bucket $2-cdp-bucket 2>&1 | wc -l) -gt 0 ] 
+if [ $(aws s3api head-bucket --bucket $bucket 2>&1 | wc -l) -gt 0 ] 
 then
-    aws s3api create-bucket --bucket $2-cdp-bucket --region $3 > /dev/null 2>&1
-    aws s3api put-object --bucket $2-cdp-bucket --key $2-dl/logs/ > /dev/null 2>&1
-    aws s3api put-object --bucket $2-cdp-bucket --key $2-dl/ranger/audit/  > /dev/null 2>&1
-    aws s3api put-object --bucket $2-cdp-bucket --key $2-dl/dataeng/ > /dev/null 2>&1
-    aws s3api put-object --bucket $2-cdp-bucket --key $2-dl/datasci/ > /dev/null 2>&1
-else 
-    aws s3api put-object --bucket $2-cdp-bucket --key $2-dl/logs/ > /dev/null 2>&1
-    aws s3api put-object --bucket $2-cdp-bucket --key $2-dl/ranger/audit/ > /dev/null 2>&1
-    aws s3api put-object --bucket $2-cdp-bucket --key $2-dl/dataeng/ > /dev/null 2>&1
-    aws s3api put-object --bucket $2-cdp-bucket --key $2-dl/datasci/ > /dev/null 2>&1
+    aws s3api create-bucket --bucket $bucket --region $3 --create-bucket-configuration LocationConstraint=$3 > /dev/null 2>&1
 fi
+
+aws s3api put-object --bucket $bucket --key $2-dl/logs/ > /dev/null 2>&1
+aws s3api put-object --bucket $bucket --key $2-dl/ranger/audit/ > /dev/null 2>&1
+aws s3api put-object --bucket $bucket --key $2-dl/dataeng/ > /dev/null 2>&1
+aws s3api put-object --bucket $bucket --key $2-dl/datasci/ > /dev/null 2>&1
 
 
 
